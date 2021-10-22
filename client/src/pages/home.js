061e866f11db9ab1b24d6974fc1ac7fc417bf033
Justin Bartlett <finnsquib@googlemail.com>
2021-10-22 15:52:35 +0100

    more tutorial done. Currently doesnt automatically show new posts without reloading the page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
 import { Grid } from 'semantic-ui-react'
 
+import { AuthContext } from '../context/auth'
 import PostCard from '../components/PostCard'
+import PostForm from '../components/PostForm'
+import { FETCH_POSTS_QUERY } from '../util/graphql'
 
 function Home() {
-  const {
-    loading,
-    data: { getPosts: posts }
-    = {} } = useQuery(FETCH_POSTS_QUERY);
+  const { user } = useContext(AuthContext)
+  const { loading, data: { getPosts: posts } = {} } =
+    useQuery(FETCH_POSTS_QUERY)
 
   return (
     <Grid columns={3}>
@@ -17,6 +18,11 @@ function Home() {
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading posts.. </h1>
         ) : (
@@ -32,27 +38,4 @@ function Home() {
   )
 }
 
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`
-
 export default Home
-//2:45:26
